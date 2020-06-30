c3e21bb4f395b22d7fa9143ecffe0b175a3b672e
Lili Manrique <liliana.manrique1@gmail.com>
2020-06-30 13:38:24 -0600

    Change ISBN to ID and start using book data in display view

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Location } from "@angular/common";
+import { BookDetailsService } from "../book-details.service";
 
 @Component({
-  selector: 'app-book-details',
-  templateUrl: './book-details.component.html',
-  styleUrls: ['./book-details.component.scss']
+  selector: "app-book-details",
+  templateUrl: "./book-details.component.html",
+  styleUrls: ["./book-details.component.scss"],
 })
 export class BookDetailsComponent implements OnInit {
+  book;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private bookDetailsService: BookDetailsService,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
+    this.getBook();
   }
 
+  getBook(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    console.log(id)
+    this.bookDetailsService.fetchBook(id)
+      .subscribe(book => {
+        console.log(book)
+        this.book = book
+      })
+  }
 }
