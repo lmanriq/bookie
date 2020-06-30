61d70ef88c1b32502c728eb9428851f273c5d22b
Lili Manrique <liliana.manrique1@gmail.com>
2020-06-30 14:37:14 -0600

    Break out id component and force refresh when URL changes.

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { Location } from "@angular/common";
 import { BookDetailsService } from "../book-details.service";
-import * as convert from 'xml-js';
+import * as convert from "xml-js";
 
 @Component({
   selector: "app-book-details",
@@ -12,21 +12,24 @@ import * as convert from 'xml-js';
 export class BookDetailsComponent implements OnInit {
   book;
   similarBooks;
-
-
+  id;
+  
   constructor(
     private route: ActivatedRoute,
     private bookDetailsService: BookDetailsService,
-    private location: Location
-  ) {}
+    private location: Location,
+    private router: Router
+  ) {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+  }
 
   ngOnInit(): void {
+    this.id = +this.route.snapshot.paramMap.get("id");
     this.getBook();
   }
 
   getBook(): void {
-    const id = +this.route.snapshot.paramMap.get("id");
-    this.bookDetailsService.fetchBook(id).subscribe((book) => {
+    this.bookDetailsService.fetchBook(this.id).subscribe((book) => {
       console.log(book);
       this.book = book;
       this.similarBooks = book.similar_books.book;
